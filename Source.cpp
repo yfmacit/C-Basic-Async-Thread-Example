d61523b829ec0099801456fd5a41fedddaa568fc
Oğuz Kahraman <ogz.khrmn@hotmail.com>
2016-10-07 11:49:31 +0300

    First Version

---
+++ Source.cpp
@@ -0,0 +1,31 @@
+ #include <future>
+ #include <iostream>
+ #include <vector>
+
+ int twice(int m,int m2) {
+
+	 //infinity loop
+	 while (1) {
+		 std::cout << m*m2 <<std::endl;
+	 }
+	 return 1;
+}
+
+ int main() {
+   std::vector<std::future<int>> futures;
+
+
+   //adding to vetor
+   for (int i = 0; i < 10; ++i) {
+     futures.push_back(std::async(twice, i,i));
+
+	}
+
+   //retrive and print the value stored in the future
+   for (auto &e : futures) {
+	   e.get();
+		 
+	}
+
+   return 0;
+}
